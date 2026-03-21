42aa87ae768f7cb22cbde891c5a62cf4ca3229d5
Tripa9 <jlloalc@etsinf.upv.es>
2026-03-21 19:45:00 +0100

    Añadida casilla de meta (controlada desde el sceneManager)

--- SocOvan/Assets/Scripts/SceneManager.cs
+++ SocOvan/Assets/Scripts/SceneManager.cs
@@ -4,18 +4,30 @@ using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
-    
-    public int[,] cells = { {10, 10, 10, 10, 10}, {10, 1, 0, 0, 10}, {10, 0, 0, 0, 10}, { 10, 0, 0, 0, 10 }, { 10, 10, 10, 10, 10 } };
+    private GameObject[] Obstacles;
+    private GameObject[] Boxes;
+    private GameObject[] Goals;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Obstacles = GameObject.FindGameObjectsWithTag("Wall");
+        Boxes = GameObject.FindGameObjectsWithTag("Pushable");
+        Goals = GameObject.FindGameObjectsWithTag("Goal");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach (var goal in Goals)
+        {
+            foreach (var box in Boxes)
+            {
+                if (goal.transform.position.x == box.transform.position.x && goal.transform.position.y == box.transform.position.y)
+                {
+                    Debug.Log("¡NIVEL COMPLETADO!");
+                }
+            }
+        }
     }
 }
