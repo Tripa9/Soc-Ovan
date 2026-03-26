c76e8aea0dadf9b1943d8212f3a75e161804043b
Tripa9 <jlloalc@etsinf.upv.es>
2026-03-26 01:49:41 +0100

    Añadida funcionalidad de diálogos

--- SocOvan/Assets/Scripts/SceneManager.cs
+++ SocOvan/Assets/Scripts/SceneManager.cs
@@ -1,9 +1,20 @@
 using Mono.Cecil;
 using System.Linq.Expressions;
 using UnityEngine;
+using TMPro;
+using System.Collections;
 
 public class SceneManager : MonoBehaviour
 {
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TMP_Text dialogueText;
+    [SerializeField] private GameObject _player;
+    [SerializeField, TextArea(4, 6)] private string[] dialogueLines;
+    private int lineIndex;
+    public float typingTime = 0.05f;
+
+    private bool ending = true;
+
     private GameObject[] Obstacles;
     private GameObject[] Boxes;
     private GameObject[] Goals;
@@ -14,6 +25,51 @@ public class SceneManager : MonoBehaviour
         Obstacles = GameObject.FindGameObjectsWithTag("Wall");
         Boxes = GameObject.FindGameObjectsWithTag("Pushable");
         Goals = GameObject.FindGameObjectsWithTag("Goal");
+
+        var movementScript = _player.GetComponent<PlayerMovement>();
+
+        movementScript.enabled = false;
+
+        StartDialogue();
+
+    }
+
+    void StartDialogue()
+    {
+        dialoguePanel.SetActive(true);
+        lineIndex = 0;
+        StartCoroutine(ShowLine());
+    }
+
+    private IEnumerator ShowLine()
+    {
+        dialogueText.text = string.Empty;
+        foreach (char ch in dialogueLines[lineIndex])
+        {
+            dialogueText.text += ch;
+            yield return new WaitForSeconds(typingTime);
+        }
+        if (dialogueText.text == dialogueLines[lineIndex])
+        {
+            yield return new WaitForSeconds(1.0f);
+            NextDialogueLine();
+        }
+    }
+
+    private void NextDialogueLine()
+    {
+        lineIndex++;
+        if (lineIndex < 3 )
+        {
+            StartCoroutine(ShowLine());
+        }
+        else
+        {
+            dialoguePanel.SetActive(false);
+
+            var movementScript = _player.GetComponent<PlayerMovement>();
+            movementScript.enabled = true;
+        }
     }
 
     // Update is called once per frame
@@ -25,9 +81,53 @@ public class SceneManager : MonoBehaviour
             {
                 if (goal.transform.position.x == box.transform.position.x && goal.transform.position.y == box.transform.position.y)
                 {
-                    Debug.Log("¡NIVEL COMPLETADO!");
+                    if (ending)
+                    {
+                        var movementScript = _player.GetComponent<PlayerMovement>();
+
+                        movementScript.enabled = false;
+                        StartFinishingLine();
+                        ending = false;
+                    }
                 }
             }
+        }
+    }
+
+    void StartFinishingLine()
+    {
+        dialoguePanel.SetActive(true);
+        StartCoroutine(ShowLineFinish());
+    }
+
+    private IEnumerator ShowLineFinish()
+    {
+        dialogueText.text = string.Empty;
+        foreach (char ch in dialogueLines[lineIndex])
+        {
+            dialogueText.text += ch;
+            yield return new WaitForSeconds(typingTime);
+        }
+        if (dialogueText.text == dialogueLines[lineIndex])
+        {
+            yield return new WaitForSeconds(1.0f);
+            NextFinishLine();
+        }
+    }
+
+    private void NextFinishLine()
+    {
+        lineIndex++;
+        if (lineIndex < dialogueLines.Length)
+        {
+            StartCoroutine(ShowLineFinish());
+        }
+        else
+        {
+            dialoguePanel.SetActive(false);
+
+            var movementScript = _player.GetComponent<PlayerMovement>();
+            movementScript.enabled = true;
         }
     }
 }
