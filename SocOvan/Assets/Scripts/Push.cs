ed6c7312ae1faaf665b9c13da3a4be6d38a1f494
Tripa9 <jlloalc@etsinf.upv.es>
2026-05-10 23:25:49 +0200

    Empujar varias cajas y manejo de portales mejorado
    Ya se pueden empujar varias cajas, incluidas algunas interacciones con los portales

--- SocOvan/Assets/Scripts/Push.cs
+++ SocOvan/Assets/Scripts/Push.cs
@@ -1,16 +1,20 @@
 using UnityEditor.Timeline;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Push : MonoBehaviour
 {
 
     private GameObject[] Obstacles;
     private GameObject[] Boxes;
+    private GameObject[] Portals;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Obstacles = GameObject.FindGameObjectsWithTag("Wall");
         Boxes = GameObject.FindGameObjectsWithTag("Pushable");
+        Portals = GameObject.FindGameObjectsWithTag("Entry Portal");
     }
 
     // Update is called once per frame
@@ -36,22 +40,50 @@ public class Push : MonoBehaviour
     {
         Vector2 newpos = new Vector2(pos.x + direction.x, pos.y + direction.y);
 
+        bool blocked = false;
+
         foreach (var obs in Obstacles)
         {
             if (obs.transform.position.x == newpos.x && obs.transform.position.y == newpos.y)
             {
-                return true;
+                blocked = true;
+                break;
             }
         }
 
-        foreach (var box in Boxes)
+        if (!blocked)
         {
-            if (box.transform.position.x == newpos.x && box.transform.position.y == newpos.y)
+            foreach (var box in Boxes)
             {
-                return true;
+                if (box != this)
+                {
+                    if (box.transform.position.x == newpos.x && box.transform.position.y == newpos.y)
+                    {
+                        blocked = !box.GetComponent<Push>().Move(direction);
+                        break;
+                    }
+                }
             }
         }
 
-        return false;
+        if (!blocked)
+        {
+            foreach (var inPortal in Portals)
+            {
+                GameObject portalBox = inPortal.GetComponent<EntryPortal>().BoxAtExit;
+                if (portalBox != null) {
+                    if (inPortal.transform.position.x == newpos.x && inPortal.transform.position.y == newpos.y)
+                    {
+                        
+                        blocked = !portalBox.GetComponent<Push>().Move(direction);
+                        if(!blocked) inPortal.GetComponent<EntryPortal>().ExitPortal.GetComponent<ExitPortal>().BoxOn = null;
+                        break;
+                    }
+                }
+                
+            }
+        }
+
+        return blocked;
     }
 }
